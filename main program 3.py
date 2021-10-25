e07518b7c631b6a01e85edeac28c45c316800942
ahmedfarou22 <74739879+ahmedfarou22@users.noreply.github.com>
2021-10-26 01:03:38 +0200

    Update main program 3.py
    Added 5 rotors to use + only using 3 rotors instead of 6 (3 inverse) + defined __work_inverse_rotor__

--- main program 3.py
+++ main program 3.py
@@ -4,6 +4,10 @@ class rotor: # a data type that represents a rotor it takes in a number and outp
         self.rotor_out = rotor_out # a list
         self.rotor_notch = rotor_notch # a letter 
     
+    # def __inverse_rotor__(self):
+    #      self.rotor_notch = "#"
+    #      return rotor(self.rotor_out, self.rotor_in, self.rotor_notch)
+
     def __auto_rotate___(self): # this function rotates the rotor once this is helpfull because after every letter is pressed the first rotor must rotate once
         self.rotor_in = self.rotor_in[1:] + self.rotor_in[:1]      #rotate the rotor_in once 
         self.rotor_out = self.rotor_out[1:] + self.rotor_out[:1]   #rotate the rotor_out once 
@@ -30,7 +34,18 @@ class rotor: # a data type that represents a rotor it takes in a number and outp
         for i in range(len(self.rotor_out)): # loop around the rotor out and when the program finds the corsponding value return it
             if self.rotor_out[i] == value:
                 return i
-
+    
+    def __work_inverse_rotor__(self,number,rotatenext): # this is the main function it has 2 main parts
+        if rotatenext == 1: # part one cheak if the rotor before sent 1 if it did then rotate the rotor once before execting the rest of the function
+            self.rotor_in = self.rotor_in[1:] + self.rotor_in[:1]
+            self.rotor_out = self.rotor_out[1:] + self.rotor_out[:1]
+        
+        value = 0  # part 2 of the function working the rotor 
+        value = self.rotor_out[number]     # store the value of the letter that is iputed in a varuble named value
+        for i in range(len(self.rotor_in)): # loop around the rotor out and when the program finds the corsponding value return it
+            if self.rotor_in[i] == value:
+                    return i
+    
 def __plug_board__(letter,inputt,outputt):    # simple yet effective plug board
     # this plugboard works in a very simple way 
     # it has 2 lists input and output 
@@ -134,17 +149,21 @@ def __iput_output__(number_or_letter): # to make the program easier all the rota
          value = alphabet[number_or_letter]
          return value
 
+################### 5 rotors ###############
+
+rotor_dictionary = {
+  "I"   : rotor(["e","k","m","f","l","g","d","q","v","z","n","t","o","w","y","h","x","u","s","p","a","i","b","r","c","j"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"q"),
+  "II"  : rotor(["a","j","d","k","s","i","r","u","x","b","l","h","w","t","m","c","q","g","z","n","p","y","f","v","o","e"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"e"),
+  "III" : rotor(["b","d","f","h","j","l","c","p","r","t","x","v","z","n","y","e","i","w","g","a","k","m","u","s","q","o"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"v"),
+  "IV"  : rotor(["o","p","q","r","x","w","v","y","u","t","a","b","z","c","d","h","s","i","j","k","l","e","g","m","n","f"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"u"),
+  "V"   : rotor(["i","k","z","w","v","u","j","t","s","r","q","l","m","n","c","d","o","p","e","f","y","x","g","h","b","a"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"b")
+}
 
 
-# ========================================================== Main program set rotors notches and plugborad =============================================================================
 
-set_rotor_one    = int(input("what number do you want to set rotor 1 to ? "))
-set_rotor_two    = int(input("what number do you want to set rotor 2 to ? "))
-set_rotor_three  = int(input("what number do you want to set rotor 3 to ? "))
+# ==========================================================  program Starts Here =============================================================================
 
 ###################### configure the plug board ###############
-
-
 print("configure the plug board")
 y_n = input("press y to contiune and n to exit the plug board configration ")
 
@@ -158,78 +177,84 @@ if y_n == "y":
     for k in range(0,13):
         inputt.append(outputt[k])
         outputt.append(inputt[k])
-    message = input("what is the message you want to encript/decript ")
-
 
 elif y_n == "n":
     print ("you did not configure a plug board we will proceed without a plug board")
     inputt  =  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     outputt =  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    message = input("what is the message you want to encript/decript ")
+
 else:
     print("you did not press y or n restart the program")
 
 
+###################### choose the 3 rotors ###############
+choose_the_first_rotor  = input("first position: choose a  rotor from the one of the five > I, II, III, IV, V : ")
+choose_the_second_rotor = input("second position: choose a  rotor from the one of the five > I, II, III, IV, V : ")
+choose_the_third_rotor  = input("third position: choose a  rotor from the one of the five > I, II, III, IV, V : ")
+
+###################### set values to the 3 rotors ###############
+set_rotor_one    = int(input("what number do you want to set rotor 1 to ? "))
+set_rotor_two    = int(input("what number do you want to set rotor 2 to ? "))
+set_rotor_three  = int(input("what number do you want to set rotor 3 to ? "))
+
+
+message = input("what is the message you want to encript/decript ")
 result = ""
 
-###################### create all rotors and assighn set values ###############
 
-firstrotor = rotor(["e","k","m","f","l","g","d","q","v","z","n","t","o","w","y","h","x","u","s","p","a","i","b","r","c","j"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"q")
+
+
+###################### create all rotors and give them set values ###############
+
+firstrotor = rotor_dictionary[choose_the_first_rotor]
 firstrotor.__set_rotor__(set_rotor_one)
 
-secondrotor =rotor(["a","j","d","k","s","i","r","u","x","b","l","h","w","t","m","c","q","g","z","n","p","y","f","v","o","e"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"e")
+secondrotor =rotor_dictionary[choose_the_second_rotor]
 secondrotor.__set_rotor__(set_rotor_two)
 
-thirdrotor = rotor(["b","d","f","h","j","l","c","p","r","t","x","v","z","n","y","e","i","w","g","a","k","m","u","s","q","o"],["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],"#")
+thirdrotor = rotor_dictionary[choose_the_third_rotor]
 thirdrotor.__set_rotor__(set_rotor_three)
 
-forthrotor = rotor(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],["b","d","f","h","j","l","c","p","r","t","x","v","z","n","y","e","i","w","g","a","k","m","u","s","q","o"],"#")
-forthrotor.__set_rotor__(set_rotor_three)
-
-fifthrotor =rotor(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],["a","j","d","k","s","i","r","u","x","b","l","h","w","t","m","c","q","g","z","n","p","y","f","v","o","e"],"#")
-fifthrotor.__set_rotor__(set_rotor_two)
-
-sixthroto = rotor(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],["e","k","m","f","l","g","d","q","v","z","n","t","o","w","y","h","x","u","s","p","a","i","b","r","c","j"],"#")
-sixthroto.__set_rotor__(set_rotor_one)
 
 
-    
-
-
-
-
+# loop the entire message
 for letter in message:
-    a = __plug_board__(letter,inputt,outputt)
-    b = __iput_output__(a) 
-    
-    ################ rotor 1 ################
-    firstrotor.__auto_rotate___()
-    c = firstrotor.__work_rotor__(b,0)
-    r1 = firstrotor.__rotate_next__()
-    
-    ################ rotor 2 ################
-    d = secondrotor.__work_rotor__(c,r1)
-    r2 = secondrotor.__rotate_next__()
+    if letter == " ":
+        result = result + " "
+    else:
+        a = __plug_board__(letter,inputt,outputt)
+        b = __iput_output__(a) 
+        ################ rotor 1 ################
+        firstrotor.__auto_rotate___()
+        c = firstrotor.__work_rotor__(b,0)
+        r1 = firstrotor.__rotate_next__()
 
-    ################ rotor 3 ################
-    e = thirdrotor.__work_rotor__(d,r2)
-    
-    ################ reflector ################
-    f = __Man_reflector__(e)
+        ################ rotor 2 ################
+        d = secondrotor.__work_rotor__(c,r1)
+        r2 = secondrotor.__rotate_next__()
 
-    ################ rotor 4 ################
-    g = forthrotor.__work_rotor__(f,r2)
+        ################ rotor 3 ################
+        e = thirdrotor.__work_rotor__(d,r2)
 
-    ################ rotor 5 ################
-    h = fifthrotor.__work_rotor__(g,r1)
+        ################ reflector ################
+        f = __Man_reflector__(e)
 
-    ################ rotor 6 ################
-    sixthroto.__auto_rotate___()
-    i = sixthroto.__work_rotor__(h,0)
+        ################ rotor 4 ################
+        g = thirdrotor.__work_inverse_rotor__(f,0)
 
-    ############## input #####################
-    j = __iput_output__(i)
-    
-    k = __plug_board__(j,inputt,outputt)
-    result = result + k
+        ################ rotor 5 ################
+        h = secondrotor.__work_inverse_rotor__(g,0)
+
+        ################ rotor 6 ################
+        i = firstrotor.__work_inverse_rotor__(h,0)
+
+        ############## input #####################
+        j = __iput_output__(i)
+        k = __plug_board__(j,inputt,outputt)
+        result = result + k
 print(result)
+
+# I I I --- 987 --- aaaa  ---->pjon
+
+# y V V IV 6,4,9 --- 200 hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+# --> cfmbjvfwaazytmdletwbwtamjcfmbjvfwaazytmdletwbwtamjcfmbjvfwaazytmdletwbwtamjcfmbjvfwaazytmdletwbwtamjcfmbjvfwaazytmdletwbwtamjcfmbjvfwaazytmdletwbwtamjcfmbjvfwaazytmdletwbwtamjcfmbjvfwaazytmdletwbwtamj
